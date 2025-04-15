3ba89c6f526ae995985efcd153fa8e581b87521d
vishalBelwal <vishalbelwal52@gmail.com>
2025-04-16 01:52:07 +0530

    added schema for zod validations

--- lib/zod.ts
+++ lib/zod.ts
@@ -21,3 +21,27 @@ export const createHackSchema = z
         message: "End date must be after start date",
         path: ["endDate"],
     });
+
+
+
+export const signupSchema = z.object({
+    userName: z.string().min(3, {
+        message: "Username must be at least 3 characters long",
+    }),
+    email: z.string().email({
+        message: "Please enter a valid email address",
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters long",
+    }),
+})
+
+export const signupFormSchema = z.object({
+    username: z.string().min(3, {
+      message: "Username must be at least 3 characters.",
+    }),
+    email: z.string().email(),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+  })

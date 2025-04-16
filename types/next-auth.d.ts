453d98d20e26f4978982efe67dfdf5abb2dcfa83
vishalBelwal <vishalbelwal52@gmail.com>
2025-04-16 14:34:24 +0530

    feat: Add NextApiResponseServerIO type definition

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -20,3 +20,16 @@ declare module 'next-auth/jwt' {
     userName?: string;
   }
 }
+
+// types/next.d.ts
+import type { Server as HTTPServer } from "http";
+import type { Socket as NetSocket } from "net";
+import type { Server as IOServer } from "socket.io";
+
+export type NextApiResponseServerIO = {
+  socket: NetSocket & {
+    server: HTTPServer & {
+      io: IOServer;
+    };
+  };
+};
